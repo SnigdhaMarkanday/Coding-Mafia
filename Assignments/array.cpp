34cbb9bbb3a7f29ed219b5faccd125dda008a596
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-06-06 00:58:15 +0530

    Update array.cpp

--- Assignments/array.cpp
+++ Assignments/array.cpp
@@ -18,5 +18,18 @@ int main(){
         s=s+arr[i];
         cout<<s<<endl;
     }
+    //Step 4 
+    char matrix[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+           cin>>matrix[i][j]; 
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+           cout<<matrix[i][j]<<" "; 
+        }
+        cout<<endl;
+    }
     return 0;
 }
