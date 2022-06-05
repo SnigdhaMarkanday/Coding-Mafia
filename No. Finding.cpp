71790a36d65c68af94b4b9da7fc67dfb9b3cb7ba
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2022-06-05 14:19:42 +0530

    Keep adding no.s from 1......n such that sum <150

---
+++ No. Finding.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Snigdha Markanday
+    int i=0,s=0;
+    while(s<150){
+        s=s+i;
+        i++;
+    }
+    cout<<i;
+    
+    return 0;
+}
